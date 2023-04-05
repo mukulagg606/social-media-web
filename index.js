6489153506dde632643ace799180a34111034f62
Mukul <mukulaggarwal606@gmail.com>
2023-04-05 16:46:52 +0530

    Initial commit:Created structured directory

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+app.set("view engine","ejs");
+app.set("views","views");
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+        return;
+    }
+    console.log(`Server is running successfully on port ${port}`);
+})

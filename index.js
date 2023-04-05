d1476225eedfd160f054b881e72c2f29c58caa2d
Mukul <mukulaggarwal606@gmail.com>
2023-04-05 16:47:09 +0530

    Initial commit:Created structured directory

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const port = 8000;
 const app = express();
 
+app.use('/',require("./routes/index.js"));
+
 app.set("view engine","ejs");
 app.set("views","views");
 
